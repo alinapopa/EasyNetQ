2eea637f4f0f4161f4f0af82bf440c720f3c3845
alinapopa <alpopa@microsoft.com>
2016-11-29 10:51:09 -0800

    Upload multiple files

--- Build/UploadTestresults.ps1
+++ Build/UploadTestresults.ps1
@@ -1,3 +1,4 @@
 ﻿# upload results to AppVeyor
 $wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", ("c:\projects\easynetq\Source\EasyNetQ.Tests\TestResult.xml"))
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", ("c:\projects\easynetq\Source\EasyNetQ.DI.Tests\TestResult.xml"))
